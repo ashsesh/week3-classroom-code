649326c214951264295503a0ccd3829aac516286
cherimarie <cherimarie@gmail.com>
2026-04-14 10:19:38 -0700

    Add create method, error handling

--- routes/cars.js
+++ routes/cars.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 const carData = require('../data/cars.json')
 
+// {"id":4,"name":"ford torino","mpg":"17","weight":"3449","origin":"usa","year":"1970"}
+router.post('/', (req, res) => {
+  if(!req.query.name || !req.query.mpg || !req.query.weight || !req.query.origin || !req.query.year){
+    res.send({message: "Please send name, mpg, weight, origin, and year as query parameters"});
+    return;
+  }
+
+  let newCar = {
+    name: req.query.name,
+    mpg: req.query.mpg,
+    weight: req.query.weight,
+    origin: req.query.origin,
+    year: req.query.year,
+    id: Math.floor(Math.random() * 90000)
+  }
+
+  res.send({data: newCar});
+
+})
+
 router.get('/', (req, res) => {
   console.log(req.query.id)
   if(!req.query.id){
@@ -16,7 +36,7 @@ router.get('/', (req, res) => {
     return;
   }
 
-  res.send(theCar);
+  res.send({data: theCar});
 })
 
 module.exports = router;
